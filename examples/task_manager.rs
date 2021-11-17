d9c9d1ac37bb3b7f36bf3c144d2391140a873485
Dmitry Pershin <dapper1291@gmail.com>
2021-11-17 20:12:21 +0500

    Library api reimplemented. Tests added.

--- examples/task_manager.rs
+++ examples/task_manager.rs
@@ -1,13 +1,20 @@
 use coachman as cm;
 use coachman::try_await;
-use coachman::{Cancelled, Completed};
+use coachman::{Canceled, Completed};
 use std::time;
 
-async fn function(i: usize) {
+async fn inner(i: usize) {
     match try_await!(tokio::time::sleep(time::Duration::from_secs(i as u64))) {
-        Cancelled => println!("task#{} cancelled", i),
-        Completed(_) => println!("task#{} completed", i),
+        Canceled => println!("task#{} inner canceled", i),
+        Completed(_) => println!("task#{} inner completed", i),
     };
+}
+
+async fn outer(i: usize) {
+    match try_await!(inner(i)) {
+        Canceled => println!("task#{} outer canceled", i),
+        Completed(_) => println!("task#{} outer completed", i),
+    }
 
     println!("task#{} canceled: {}", i, cm::is_task_canceled());
 }
@@ -18,22 +25,19 @@ async fn main() {
 
     let mut task_keys = Vec::new();
     for i in 0..10 {
-        let task_key = task_manager.try_spawn(function(i)).unwrap();
+        let task_key = task_manager.try_spawn(outer(i)).unwrap();
         task_keys.push(task_key)
     }
 
     tokio::time::timeout(time::Duration::from_secs(5), task_manager.process()).await;
 
-    let mut task_handles = Vec::new();
     for task_key in task_keys {
-        if let Some(handle) = task_manager.cancel_task(task_key) {
-            task_handles.push(handle);
+        if task_manager.cancel_task(task_key).is_ok() {
+            println!("task-{} canceled", task_key)
         } else {
             println!("task-{} already finished", task_key)
         }
     }
 
-    for task_handle in task_handles {
-        task_handle.await;
-    }
+    task_manager.join(true).await;
 }
